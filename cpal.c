ab163718eb6468e011e9d70b91e9e283a764f3d8
alber461 <alber461@umn.edu>
2019-06-16 14:34:07 -0500

    program successfully does nothing

---
+++ cpal.c
@@ -0,0 +1,22 @@
+#pragma fidne#
+#define NF
+#define lit typedef
+#define tni int 
+#pragma \
+0 fi#
+;typedef int til;
+;main (a) NF
+{
+#define FN(a) til
+#warning gninraw# \
+lit (a)NF enifed#
+}
+FN (a) niam;
+;lit tni fedepyt;
+#if 0
+\ amgarp#
+stup puts enifed#
+tni int enifed#
+fedepyt til enifed#
+FN enifed#
+#endif amgarp#
